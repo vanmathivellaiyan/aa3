65809549bf81dfd5ad809b9b9b007d29792d7acc
vanmathivellaiyan <vanmathivellaiyan1995@gmail.com>
2016-07-30 09:38:18 +0530

    Add files via upload

---
+++ lengthstring.java
@@ -0,0 +1,11 @@
+package guviprogram;
+
+public class lengthstring {
+	public static void main(String[] args){
+	String str=("hello fgdf");
+	String str1=("haifd fdg");
+	System.out.println(str.length());
+	System.out.println(str1.length());
+	}
+
+}
